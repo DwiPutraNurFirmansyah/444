dc8bf96af17e9c51590dbf33fe28b0b1b51aa570
DwiPutraNurFirmansyah <nurfirmans1@gmail.com>
2024-11-10 15:09:23 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,49 @@
+<?php
+    include 'header.php';
+?>
+
+<div class="container" style="margin-top: 20px; padding-bottom: 300px;">
+    <!-- Title -->
+    <h2 class="section-title" style="border-bottom: 4px solid #ff8680; margin-bottom: 20px; padding-top: 10px;"><b>Layanan</b></h2>
+    <p class="text-justify">
+        Kami menyediakan berbagai layanan yang dapat memenuhi kebutuhan Anda. Berikut adalah informasi terkait layanan kami:
+    </p>
+
+    <!-- Info Section -->
+    <div class="row">
+        <div class="col-md-6">
+            <ul>
+                <li><strong>Jam Buka:</strong> Senin - Minggu, 08:00 - 21:00</li>
+                <li><strong>Alamat Pusat:</strong> Jl. Alamat No. 123, Kota Besar, Indonesia</li>
+            </ul>
+        </div>
+        
+        <div class="col-md-6">
+            <strong>Outlet yang Tersedia:</strong>
+            <ul>
+                <li>Jakarta: Jl. Thamrin No. 45, Jakarta Pusat</li>
+                <li>Bandung: Jl. Dago No. 78, Bandung</li>
+                <li>Surabaya: Jl. Tunjungan No. 9, Surabaya</li>
+                <li>Bali: Jl. Sunset Road No. 12, Kuta, Bali</li>
+                <li>Yogyakarta: Jl. Malioboro No. 27, Yogyakarta</li>
+                <li>Semarang: Jl. Pandanaran No. 30, Semarang</li>
+                <li>Medan: Jl. Gajah Mada No. 99, Medan</li>
+                <li>Makassar: Jl. Pettarani No. 5, Makassar</li>
+            </ul>
+        </div>
+    </div>
+
+    <!-- Google Maps Embed -->
+    <div class="row">
+        <div class="col-md-12">
+            <h3 class="section-title">Lokasi Pusat</h3>
+            <div class="embed-responsive embed-responsive-16by9">
+                <iframe class="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3945.999093510444!2d106.82495315051048!3d-6.208800692784118!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f2c3cf8ff0f9%3A0xa32544e56b5f5ecb!2sJl.%20Contoh%20Alamat%20No.%20123%2C%20Kota%20Besar%2C%20Indonesia!5e0!3m2!1sen!2sid!4v1698195984423!5m2!1sen!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+    include 'footer.php';
+?>
